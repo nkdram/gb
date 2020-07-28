62ea26f186f94c44fab339d943d4a92086e3ee34
nkdram <ramkumar.nambhi@al-enterprise.com>
2020-07-28 15:21:11 +0200

    Fixing Unicorn and Assembly changes

--- init.ps1
+++ init.ps1
@@ -3,15 +3,16 @@ Get-Content ".env" | foreach-object -begin {$settings=@{}} -process { $value = [
 # Creating required SSL certificates
 Write-Host "Creating required certificate..." -ForegroundColor Green
 Push-Location .\.docker\traefik\certs
-mkcert $settings.HOSTNAME
+New-SelfSignedCertificate  -Subject "CN=${HOSTNAME_CM}" -Type CodeSigningCert -KeySpec "Signature" -KeyUsage "DigitalSignature" -FriendlyName "${HOSTNAME_CM}" -NotAfter (get-date).AddYears(5) 
+New-SelfSignedCertificate  -Subject "CN=${HOSTNAME_CD}" -Type CodeSigningCert -KeySpec "Signature" -KeyUsage "DigitalSignature" -FriendlyName "${HOSTNAME_CD}" -NotAfter (get-date).AddYears(5) 
 Pop-Location
 
 # Build all containers in the Sitecore instance, forcing a pull of latest base containers
 Write-Host "Building containers..." -ForegroundColor Green
 #docker-compose build
-if ($LASTEXITCODE -ne 0) {
-    Write-Error "Container build failed, see errors above."
-}
+#if ($LASTEXITCODE -ne 0) {
+    #Write-Error "Container build failed, see errors above."
+#}
 
 # Start the Sitecore instance
 Write-Host "Starting Sitecore environment..." -ForegroundColor Green
