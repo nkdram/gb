50d6249eeea82ca06c44d72a38064756eba1d798
nkdram <nkdramkumar@gmail.com>
2020-07-31 15:41:43 +0200

    Page creation and bucket rules

---
+++ back-end/Foundation/Rules/website/Buckets/CreateBucketBasedOnDealerId.cs
@@ -0,0 +1,54 @@
+﻿// Sitecore.Buckets.Rules.Bucketing.Actions.CreateItemNameBasedPath<T>
+using Sitecore.Buckets.Rules.Bucketing;
+using Sitecore.Buckets.Util;
+using Sitecore.Diagnostics;
+using Sitecore.Rules.Actions;
+
+
+namespace Globags.Foundation.Rules.Buckets
+{
+    /// <summary>
+    /// Defines the run class.
+    /// </summary>
+    /// <typeparam name="T">The rule context.</typeparam>
+    public class CreateBucketBasedOnProductId<T> : RuleAction<T> where T : BucketingRuleContext
+    {
+        /// <summary>
+        /// Gets or sets the number of levels
+        /// </summary>
+        /// <value>The script id.</value>
+        public string Levels
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Executes the specified rule context.
+        /// </summary>
+        /// <param name="ruleContext">The rule context.</param>
+        public override void Apply(T ruleContext)
+        {
+            Assert.ArgumentNotNull(ruleContext, "ruleContext");
+
+            if (!ruleContext.BucketItem.Fields[Templates.ProductPage.ProductID]?.HasValue ?? true)
+                return;
+
+            //Get ProductID
+            var productId = ruleContext.BucketItem.Fields[Templates.ProductPage.ProductID].Value;
+            if (int.TryParse(Levels, out int result))
+            {
+                if (result > productId.Length)
+                {
+                    result = productId.Length;
+                }
+                char[] values = productId.Substring(0, result).ToCharArray();
+                ruleContext.ResolvedPath = string.Join(Constants.ContentPathSeperator, values).ToLowerInvariant();
+            }
+            else
+            {
+                Log.Warn("CreateBucketBasedOnProductId: Cannot resolve item path by this rule", this);
+            }
+        }
+    }
+}
---
+++ back-end/Foundation/Rules/website/Templates.cs
@@ -0,0 +1,23 @@
+﻿using Sitecore.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Globags.Foundation.Rules
+{
+    public static class Templates
+    {
+        /// <summary>
+        /// Produc page template item
+        /// </summary>
+        public static class ProductPage
+        {
+            /// <summary>
+            /// Product Page Product ID field
+            /// </summary>
+            public static readonly ID ProductID = new ID("{27BAD8C9-A21C-4CE7-BE29-8545F58D7CC2}");
+        }
+    }
+}
